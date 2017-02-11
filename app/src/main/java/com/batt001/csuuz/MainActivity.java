ae14a840435d3622f2488b3569d72a1a3e9a44e7
batt001 <batt001.master@gmail.com>
2017-02-11 23:13:23 +0100

    Added FAB basic functionality

--- app/src/main/java/com/batt001/csuuz/MainActivity.java
+++ app/src/main/java/com/batt001/csuuz/MainActivity.java
@@ -2,19 +2,21 @@ package com.batt001.csuuz;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Context applicationContext;
 
-    ImageButton home_button;
-    ImageButton city_button;
+    ImageButton homeButton;
+    ImageButton cityButton;
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         applicationContext = getApplicationContext();
+
+        setUpFloatingActionButton();
         setUpButtons();
 
     }
 
     private void setUpButtons()
     {
-        home_button = (ImageButton) findViewById(R.id.home_button);
-        city_button = (ImageButton) findViewById(R.id.city_button);
+        homeButton = (ImageButton) findViewById(R.id.home_button);
+        cityButton = (ImageButton) findViewById(R.id.city_button);
 
-        home_button.setOnClickListener(new View.OnClickListener() {
+        homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Replace with launching the other activity
@@ -42,10 +46,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        city_button.setOnClickListener(new View.OnClickListener() {
+        cityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(applicationContext, "Pressed city", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void setUpFloatingActionButton(){
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floating_action_button);
+        floatingActionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // Click action
+                // Launch new activity here
+                // Intent intent = new Intent(applicationContext, NewMessageActivity.class);
+                // startActivity(intent);
+                Toast.makeText(applicationContext,"FAB pressed", Toast.LENGTH_SHORT).show();
             }
         });
     }
